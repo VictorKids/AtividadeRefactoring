41e74a275a90cc2aabe2489d7f7d8b9bce2f9aad
Victor <48036482+VictorKids@users.noreply.github.com>
2022-02-01 18:49:29 +0000

    Commit 10

--- Rental.java
+++ Rental.java
@@ -21,12 +21,7 @@ public class Rental {
   } 
 
   public int getFrequentRenterPoints(){
-    int frequentRenterPoints = 1;
-    // add bonus for a two day new release rental
-    if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-      this.getDaysRented() > 1) frequentRenterPoints ++;
-
-    return frequentRenterPoints;
+    return _movie.getFrequentRenterPoints(_daysRented);
   }
 
 }
